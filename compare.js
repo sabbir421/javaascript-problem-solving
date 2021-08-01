2bbb3a5af467417a55837b7e9164296ff99609e4
sabbir421 <sabbiralazim421@gmail.com>
2021-08-02 02:08:06 +0600

    compare

---
+++ compare.js
@@ -0,0 +1,12 @@
+var firstName = "sabbir";
+var lastName = "shehab";
+console.log(firstName==lastName);
+var number1 = 20;
+var number2 = 20;
+console.log(number1==number2);
+console.log(number1>number2);
+console.log(number1>number2);
+console.log(number1<number2);
+console.log(number1 !=number2);
+console.log(number1 <=number2);
+
