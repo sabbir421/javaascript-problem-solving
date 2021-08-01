3ff39fdf9c9341c9082ae75cdfffb6bc325c01a0
sabbir421 <sabbiralazim421@gmail.com>
2021-08-02 01:48:26 +0600

    index solve

---
+++ array-index.js
@@ -0,0 +1,6 @@
+var numbers = [5, 6, 7, 8, 9,];
+console.log(numbers.indexOf(5));
+console.log(numbers.indexOf(6));
+console.log(numbers.indexOf(7));
+console.log(numbers.indexOf(8));
+console.log(numbers.indexOf(9));
