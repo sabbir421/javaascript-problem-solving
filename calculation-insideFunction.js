c448f6e1dc9e1701d34d65fb7a657d7792fad8ec
sabbir421 <sabbiralazim421@gmail.com>
2021-08-05 10:38:57 +0600

    calculation-inside-function

---
+++ calculation-insideFunction.js
@@ -0,0 +1,28 @@
+function add(num1,num2){
+    var result= num1 + num2;
+    return result;
+}
+var total = add(5,5);
+console.log(total);
+
+function sub(num1,num2){
+    var result= num1 - num2;
+    return result;
+}
+var total = sub(5,5);
+console.log(total);
+
+function multiplication(num1,num2){
+    var result= num1 * num2;
+    return result;
+}
+var total =multiplication(5,5);
+console.log(total);
+
+function division(num1,num2){
+    var result= num1 / num2;
+    return result;
+}
+var total = division(5,5);
+console.log(total);
+
