f771782698cae29ecb3133d4eb1f49c5c2ae1725
sabbir421 <sabbiralazim421@gmail.com>
2021-08-08 04:06:29 +0600

    small number

---
+++ smallestNumber-array.js
@@ -0,0 +1,13 @@
+var array= [20,10,1,4,5,6];
+let minimum = array[0] // let at first 0 index is small number
+for (let i = 0; i < array.length; i++) {
+    var element=array[i]
+    if (element<minimum) {
+        minimum=element; // if condition will be true then element value swap into minimum
+        
+        
+    }
+   
+    
+} console.log(minimum);
+
