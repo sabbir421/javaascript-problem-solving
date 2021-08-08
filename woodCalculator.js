47b489cebb8aaa05958d51afa901303920b1d872
sabbir421 <sabbiralazim421@gmail.com>
2021-08-08 06:15:42 +0600

    wood calculator

---
+++ woodCalculator.js
@@ -0,0 +1,10 @@
+
+function woodCalculator(chairQuantity, tabilQuantity, bedQuantity) {
+    let chair = 3 *chairQuantity;
+let tabil = 10 * tabilQuantity;
+let bed = 50 * bedQuantity;
+totalWood = chair + tabil + bed ;
+return totalWood;
+}
+ let yourWoodQuantity = woodCalculator(2,2,2);
+ console.log(yourWoodQuantity);
